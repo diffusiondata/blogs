3563adfc6b4a492ec21f820d487a0c3d8c8e51f0
Philip Aston <philipa@mail.com>
2016-04-21 15:20:45 +0100

    Emacs font locking for Security.store.

---
+++ Security/emacs/README.md
@@ -0,0 +1,8 @@
+Emacs modes for editing Diffusion security files
+================================================
+
+# diffusion-system-authentication-mode
+
+A simple Emacs mode for editing Diffusion system authentication files.
+
+The mode provides syntax highlighting.
---
+++ Security/emacs/diffusion-system-authentication-mode.el
@@ -0,0 +1,47 @@
+;;-*- coding: utf-8 -*-
+;; diffusion-system-authentication-mode.el -- highlighting for Diffusion SystemAuthentication.store files
+;;
+;; Copyright (C) 2016 Push Technology, Ltd
+;; Author: Philip Aston
+;; Keywords: faces
+
+
+(defgroup diffusion-system-authentication-mode nil
+  "View Diffusion system authentication files.")
+
+(defgroup diffusion-system-authentication-mode-faces nil
+  "Faces for diffusion-system-authentication-mode."
+  :prefix "diffusion-system-authentication-"
+  :group 'diffusion-system-authentication-mode)
+
+(defface diffusion-system-authentication-delimiter-face
+  '((((class color)) (:foreground "gray75")))
+  "Face for delimeters."
+  :group 'diffusion-system-authentication-faces)
+
+(defcustom diffusion-system-authentication-mode-hook
+  nil
+  "Normal hook run when starting to view a Diffusion system authentication file."
+  :type 'hook
+  :group 'diffusion-system-authentication-mode)
+
+(setq diffusion-system-authentication-mode-font-lock-defaults
+      (list
+       `(,(regexp-opt '("add" "principal" "allow" "deny" "abstain" "anonymous" "connections" "password" "roles" "set" "verify" "encrypted" ) 'words) . font-lock-keyword-face)
+       `(,(regexp-opt '("," "[" "]")) . 'diffusion-system-authentication-delimiter-face)
+        ))
+
+(define-derived-mode diffusion-system-authentication-mode fundamental-mode "Diffusion System Authentication"
+  "Major mode for viewing Diffusion system authentication files.
+Currently only provides syntax highlighting."
+  (setq font-lock-defaults
+        '(diffusion-system-authentication-mode-font-lock-defaults nil t))
+  (font-lock-fontify-buffer)
+  (modify-syntax-entry ?\; "< b")
+  (modify-syntax-entry ?\n "> b")
+  )
+
+(add-to-list 'auto-mode-alist
+             '("SystemAuthentication.store\\'" . diffusion-system-authentication-mode))
+
+(provide 'diffusion-system-authentication-mode)
